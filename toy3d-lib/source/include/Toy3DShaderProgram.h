435abe8a63419aee9f02195edc7644db10e0a9fc
pngliu <pngliu@gmail.com>
2012-12-06 08:56:36 +0000

    add toy3d-lib toy3d-examples

---
+++ toy3d-lib/source/include/Toy3DShaderProgram.h
@@ -0,0 +1,26 @@
+
+#ifndef _TOY3D_SHADER_PROGRAM_H
+#define _TOY3D_SHADER_PROGRAM_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "Toy3DCommon.h"
+
+TOY3D_BEGIN_NAMESPACE
+
+    class ShaderProgram
+    {
+ 	    protected:
+
+        public:
+            ShaderProgram();
+            ~ShaderProgram();
+    };
+
+
+TOY3D_END_NAMESPACE
+
+#endif 
---
+++ toy3d-lib/source/src/Toy3DRenderer.cpp
@@ -0,0 +1,16 @@
+
+
+#include "Toy3DRenderer.h"
+
+TOY3D_BEGIN_NAMESPACE
+
+    Renderer::Renderer() 
+    {
+    }
+
+    Renderer::~Renderer() 
+    {
+    }
+
+
+TOY3D_END_NAMESPACE
