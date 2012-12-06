60b575f891a940d57597d73d6895d881a1a3d53c
pngliu <pngliu@gmail.com>
2012-12-06 06:11:03 +0000

    Import initial project code

---
+++ source/include/Toy3DMesh.h
@@ -0,0 +1,23 @@
+
+#ifndef _TOY3D_MESH_H
+#define _TOY3D_MESH_H
+
+#include "Toy3DCommon.h"
+
+
+TOY3D_BEGIN_NAMESPACE
+
+    class Mesh 
+    {
+
+    public:
+        Mesh();
+        ~Mesh();
+
+    };
+
+
+
+TOY3D_END_NAMESPACE
+
+#endif
