ec52cd8af7efa672dfcb232ae6f77c78db20bde7
pngliu@gmail.com <pngliu@gmail.com@c1c7c57f-1dc5-8778-2b2c-6d825f247b2d>
2013-01-04 10:06:30 +0000

    add Toy3DEntity.h Toy3DEntity.cpp Toy3DMaterial.h Toy3DMaterial.cpp

---
+++ trunk/toy3d-lib/source/src/Toy3DMaterial.cpp
@@ -0,0 +1,41 @@
+
+#include "Toy3DMaterial.h"
+
+
+TOY3D_BEGIN_NAMESPACE
+
+    Material::Material() 
+    {
+        mShaderProgram = NULL;
+        mTexture = NULL;
+    }
+
+
+    Material::~Material() 
+    {
+        FREEANDNULL(mShaderProgram);
+        FREEANDNULL(mTexture);
+    }
+
+
+    void Material::setShaderProgram (ShaderProgram *prog)
+    {
+       mShaderProgram = prog; 
+    }
+
+    ShaderProgram* Material::getShaderProgram ()
+    {
+       return mShaderProgram; 
+    }
+
+
+
+    void Material::setTexture (Texture *tex)
+    {
+        mTexture = tex;
+    }
+
+
+
+
+TOY3D_END_NAMESPACE
