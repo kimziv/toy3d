407c67effed3e2318161d0df4c4e995ed0503c36
pngliu <pngliu@gmail.com>
2013-04-07 07:58:48 +0000

    add FrameListener

---
+++ toy3d-lib/source/include/Toy3DFrameListener.h
@@ -0,0 +1,30 @@
+
+#ifndef _TOY3D_FRAME_LISTENER_H
+#define _TOY3D_FRAME_LISTENER_H
+
+#include "Toy3DCommon.h"
+
+
+TOY3D_BEGIN_NAMESPACE
+
+    class FrameListener 
+    {
+
+
+    public:
+        virtual ~FrameListener() {}
+
+        virtual bool frameStarted() 
+        {return TRUE;}
+
+        virtual bool frameEnded() 
+        {return TRUE;}
+
+
+    };
+
+
+
+TOY3D_END_NAMESPACE
+
+#endif
